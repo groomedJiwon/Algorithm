ae53a89b46f247f0c0f4c569f7f3d91183444314
groomedJiwon <106893781+groomedJiwon@users.noreply.github.com>
2022-06-30 17:02:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+
+int main()
+
+{
+    char input;
+    cin >> input;
+    int output = input;
+    cout << output;
+
+   
+
+
+    return 0;
+}
