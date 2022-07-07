309b4be580d566b0a662e211d045fa5fad1d21df
groomedJiwon <106893781+groomedJiwon@users.noreply.github.com>
2022-07-07 16:45:55 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 180 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -4,39 +4,31 @@
 #include <vector>
 using namespace std;
 
-bool isBlank(int num, int i, int j) {
-    if (((i > num / 3) && (i <= num * 2 / 3)) && ((j > num / 3) && (j <= num * 2 / 3))) return true; 
-    else if (num > 1) return isBlank(num / 3, i % (num / 3), j % (num / 3)); 
-    else return false;
-
-
-}
-
-void hollSquare(int num) {
-    for (int i = 1; i <= num; i++) {
-        for (int j = 1; j <= num; j++) {
-            if (isBlank(num, i, j)) std::cout << " ";
-            else std::cout << "*";
-        }
-        std::cout << "\n";
-    }
+void Star(int x, int y, int N)
+{
+	if (x % 3 == 1 && y % 3 == 1)
+		cout << " ";
+	else if (N / 3 == 1)
+		cout << "*";
+	else
+		Star(x / 3, y / 3, N / 3);
 }
 
 int main()
-
 {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 
+	int N;
+	cin >> N;
 
-    ios_base::sync_with_stdio(false);
+	for (int x = 0; x < N; x++)
+	{
+		for (int y = 0; y < N; y++)
+			Star(x, y, N);
 
-    cin.tie(NULL);
+		cout << '\n';
+	}
 
-    int num;
-    cin >> num;
-
-    hollSquare(num);
-
-
-
-
+	return 0;
 }
